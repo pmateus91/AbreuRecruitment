de092bcdb2e4ce32e33a92a26a3107f19346e37f
Pedro Mateus <pedromateus91@gmail.com>
2024-06-14 14:33:13 +0100

    add new enpoints and documentation to ArtGalleryController

--- VARecruitmentWebAPI/Program.cs
+++ VARecruitmentWebAPI/Program.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using VAArtGalleryWebAPI.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,7 +8,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+
+    c.IncludeXmlComments(xmlPath);
+});
 
 builder.Services.AddRepositories(builder.Configuration);
 
--- VARecruitmentWebAPI/WebApi/Controllers/ArtGalleryController.cs
+++ VARecruitmentWebAPI/WebApi/Controllers/ArtGalleryController.cs
@@ -1,15 +1,24 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using VAArtGalleryWebAPI.Application.Commands;
 using VAArtGalleryWebAPI.Application.Queries;
 using VAArtGalleryWebAPI.WebApi.Models;
 
 namespace VAArtGalleryWebAPI.WebApi.Controllers
 {
+    /// <summary>
+    /// Controller for managing Art Galleries.
+    /// </summary>
     [Route("api/art-galleries")]
     [ApiController]
     public class ArtGalleryController(IMediator mediator) : ControllerBase
     {
+        /// <summary>
+        /// Gets all art galleries.
+        /// </summary>
+        /// <returns>List of art galleries.</returns>
         [HttpGet]
+        [Route("GetAllGalleries")]
         public async Task<ActionResult<List<GetArtGalleryResult>>> GetAllGalleries()
         {
             var galleries = await mediator.Send(new GetAllArtGalleriesQuery());
@@ -19,10 +28,91 @@ namespace VAArtGalleryWebAPI.WebApi.Controllers
             return Ok(result);
         }
 
-        [HttpPost]
-        public async Task<ActionResult<SaveArtGalleryResult>> Create([FromBody] CreateArtGalleryRequest request)
+        /// <summary>
+        /// Gets an art gallery by its ID.
+        /// </summary>
+        /// <param name="artGalleryId">The ID of the art gallery.</param>
+        /// <returns>The requested art gallery.</returns>
+        [HttpGet]
+        [Route("GetArtGalleryById")]
+        public async Task<ActionResult<GetArtGalleryResult>> GetArtGalleryByIdAsync(Guid artGalleryId)
         {
-            return StatusCode(StatusCodes.Status500InternalServerError, "Alguém vai ter de o implementar :)");
+            var result = await mediator.Send(new GetArtGalleryByIdQuery(artGalleryId));
+            if (result is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+        }
+
+        /// <summary>
+        /// Gets all ArtWorks in a specific art gallery.
+        /// </summary>
+        /// <param name="artGalleryId">The ID of the art gallery.</param>
+        /// <returns>List of artworks in the specified art gallery.</returns>
+        [HttpGet]
+        [Route("GetArtGalleryArtWorks")]
+        public async Task<ActionResult<IEnumerable<GetArtWorkResult>>> GetArtGalleryArtWorksAsync(Guid artGalleryId)
+        {
+            var result = await mediator.Send(new GetArtGalleryArtWorksQuery(artGalleryId));
+            if (result is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+        }
+
+        /// <summary>
+        /// Creates a new art gallery.
+        /// </summary>
+        /// <param name="request">The request object containing the details of the new art gallery.</param>
+        /// <returns>The created art gallery.</returns>
+        [HttpPost]
+        public async Task<ActionResult<SaveArtGalleryResult>> CreateArtGalleryAsync([FromBody] CreateArtGalleryRequest request)
+        {
+            var result = await mediator.Send(new CreateArtGalleryCommand(request));
+            if (result is null)
+            {
+                return BadRequest();
+            }
+
+            return Ok(result);
+        }
+
+        /// <summary>
+        /// Updates an existing art gallery.
+        /// </summary>
+        /// <param name="request">The request object containing the updated details of the art gallery.</param>
+        /// <returns>The updated art gallery.</returns>
+        [HttpPut]
+        public async Task<ActionResult<SaveArtGalleryResult>> UpdateArtGalleryAsync([FromBody] UpdateArtGalleryRequest request)
+        {
+            var result = await mediator.Send(new UpdateArtGalleryCommand(request));
+            if (result is null)
+            {
+                return BadRequest();
+            }
+
+            return Ok(result);
+        }
+
+        /// <summary>
+        /// Deletes an art gallery.
+        /// </summary>
+        /// <param name="artGalleryId">The ID of the art gallery to delete.</param>
+        /// <returns>A boolean indicating whether the deletion was successful.</returns>
+        [HttpDelete]
+        public async Task<ActionResult<bool>> DeleteArtGalleryAsync(Guid artGalleryId)
+        {
+            var result = await mediator.Send(new DeleteArtGalleryCommand(artGalleryId));
+            if (!result)
+            {
+                return BadRequest(result);
+            }
+
+            return Ok(result);
         }
     }
 }
